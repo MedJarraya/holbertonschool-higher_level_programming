e02b8b6b1b018c5456d611a0cf7bbb9c66c8bbb8
Mohamed Jarraya <blambertn@gmail.com>
2021-01-19 15:57:30 -0800

    last fix

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-def print_last_digit(a):
-    if a < 0:
-        nbr = (-a) % 10
-    elif a == 0:
-        nbr = a
+def print_last_digit(number):
+    if number < 0:
+        print("{:d}".format(-number % 10), end='')
+        return(-number % 10)
     else:
-        nbr = a % 10
-    print("{:d}".format(nbr), end='')
-return(nbr)
+        print("{:d}".format(number % 10), end='')
+        return(number % 10)
