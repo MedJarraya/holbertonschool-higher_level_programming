05e7159ebfe79af4209048aac4c4d36cc31561e6
Mohamed Jarraya <blambertn@gmail.com>
2021-07-12 09:34:12 -0700

    SQL

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+-- This script lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- Results should display both the score and the name (in this order)
+-- Records should be ordered by score (top first)
+SELECT score FROM second_table score >= 10;
