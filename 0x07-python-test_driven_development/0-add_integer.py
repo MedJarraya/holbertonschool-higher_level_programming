193837966e71edafd1fbbc676eac5f48a0ec27cb
Mohamed Jarraya <blambertn@gmail.com>
2021-07-12 10:36:32 -0700

    Py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-'''Adds 2 integer'''
+"""Function to add two integers"""
 
 
 def add_integer(a, b=98):
-    '''adds 2 integers.'''
+    """Add two integers
+    If a or b is a float, the fractional part is truncated before adding.
+    Args:
+        a (int or float): first operand
+        b (int or float: second operand
+    Returns:
+        int: a + b
+    """
 
-    if type(a) not in [int, float]:
-        raise TypeError("a must be an integer")
-
-    if type(b) not in [int, float]:
-        raise TypeError("b must be an integer")
-
-    return int(a) + int(b)
+    if isinstance(a, float):
+        a = int(a)
+    elif not isinstance(a, int):
+        raise TypeError('a must be an integer')
+    if isinstance(b, float):
+        b = int(b)
+    elif not isinstance(b, int):
+        raise TypeError('b must be an integer')
+    return a + b
