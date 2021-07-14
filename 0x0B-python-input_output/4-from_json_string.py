262be12539d6e28c45f252feaead4362382cdbd9
Mohamed Jarraya <blambertn@gmail.com>
2021-07-14 08:28:51 -0700

    Py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This module defines the function from_json_string. """
+import json
+
+
+def from_json_string(my_str):
+    """ This function returns an object represented
+    by a json string. """
+    return json.loads(my_str)
