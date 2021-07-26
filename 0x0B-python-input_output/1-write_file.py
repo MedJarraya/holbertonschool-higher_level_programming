bfb977c8b621570046b779246a5b4d0e39954391
Mohamed Jarraya <blambertn@gmail.com>
2021-07-26 15:20:58 -0700

    fixing

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,3 +6,4 @@ def write_file(filename="", text=""):
     """ This function opens a file and writes text to it. """
     with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
+g
--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-""" To JSON string """
+"""
+JSON representation of an object (string)
+"""
 import json
 
 
 def to_json_string(my_obj):
-    """ Function that returns the JSON representation
-    of an object (string) """
-    return (json.dumps(my_obj))
+    """
+    returns the JSON representation of an object (string)
+    """
+    return json.dumps(my_obj)
