b01cec2170f4615646dc0fe346c164b4e48067ec
Ubuntu <blambertn@gmail.com>
2021-09-19 18:25:45 -0700

    fix

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,2 @@
 #!/usr/bin/node
-const myVar = 'C is fun';
-const myVar1 ='\nPython is cool';
-const myVar2 ='\nJavaScript is amazing';
-console.log(myVar,myVar1,myVar2);
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
