43a27de1dd8c7b55047b38a9d13976724347831d
MedJarraya <blambertn@gmail.com>
2021-10-04 01:56:49 -0700

    fix

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-function add (z, x) {
-    return z + x;
+function add (a, b ){
+    return a + b;
   }
-  const z = Number(process.argv[2]);
-  const x = Number(process.argv[3]);
-  console.log(add(z, x));
+  const a = Number(process.argv[2]);
+  const b = Number(process.argv[3]);
+  console.log(add(a, b));
