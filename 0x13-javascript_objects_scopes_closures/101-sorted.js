57e8b39f14213f580cba0b31ee0f683d066f4935
MedJarraya <blambertn@gmail.com>
2021-10-07 07:15:11 -0700

    fix

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
+
+const dict = require('./101-data').dict;
+const newDict = {};
+for (const i in dict) {
+  /* i is the key */
+  if (newDict[dict[i]] === undefined) {
+    newDict[dict[i]] = [];
   }
+  newDict[dict[i]].push(i);
 }
 console.log(newDict);
