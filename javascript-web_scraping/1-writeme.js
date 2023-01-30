0b908dc93d4b51bc2a64ae7603172289ee32c2aa
Mohamed Jarraya <mohamedjarraya@192.168.1.114>
2023-01-30 16:14:35 +0100

    new

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Write me
+const fs = require('fs');
+const args = process.argv;
+const filename = args[2];
+const text = args[3];
+fs.writeFile(filename, text, 'utf-8', function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
